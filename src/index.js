05638e47b0a9739d7e3de1659c55f638ea6c1260
RaviGahire <ravigahire3@gmail.com>
2026-05-01 15:05:26 +0530

    feat: update title in index.html and enhance index.js with context providers

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import {App} from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import { App } from "./App";
+import { ProjectProvider } from "./context/ProjectContext";
+import { TaskProvider } from "./context/TaskContext";
+import { CommentProvider } from "./context/CommentContext";
+import { DashboardProvider } from "./context/DashboardContext";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ProjectProvider>
+      <TaskProvider>
+        <CommentProvider>
+          <DashboardProvider>
+            <App />
+          </DashboardProvider>
+        </CommentProvider>
+      </TaskProvider>
+    </ProjectProvider>
+  </React.StrictMode>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
